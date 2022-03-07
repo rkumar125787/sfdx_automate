de6a0ce2088fc26a0d0ff0885a2d2765e7bf8d3f
Rajesh Kumar <rkumar125787@gmail.com>
2022-03-07 21:35:19 +0530

    Changed

--- delete.sh
+++ delete.sh
@@ -13,6 +13,7 @@ done
 echo $INCLUSIONLIST
 
 #*****Checkout to Git Source Branch and Find the difference of a file in comma seprated way
+git checkout master
 git  diff --diff-filter=D --name-status origin/master...origin/sfdx $INCLUSIONLIST
 git  diff --diff-filter=D --name-only origin/master...origin/sfdx $INCLUSIONLIST | tr '\n' ',' | sed 's/\(.*\),/\1 /' > delDir.txt
 #Check if difference exist using file contents
